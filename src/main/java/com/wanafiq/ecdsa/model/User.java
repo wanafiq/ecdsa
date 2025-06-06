60269325eedebe9a38ba7a04406017e0acd7440d
Wan <wanmuhammadafiqwanshukry@gmail.com>
2025-06-07 03:18:59 +0800

    (feat) add User model and repository

---
+++ src/main/java/com/wanafiq/ecdsa/model/User.java
@@ -0,0 +1,52 @@
+package com.wanafiq.ecdsa.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@Entity
+@Table(name = "users")
+public class User extends Audit {
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "activated_at")
+    private Instant activatedAt;
+
+    @Column(name = "account_non_expired")
+    private boolean accountNonExpired;
+
+    @Column(name = "account_non_locked")
+    private boolean accountNonLocked;
+
+    @Column(name = "credentials_non_expired")
+    private boolean credentialsNonExpired;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<Role> roles;
+
+}
