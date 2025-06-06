84686a20cc071a9dd1d4add5e78a265932956399
Wan <wanmuhammadafiqwanshukry@gmail.com>
2025-06-07 03:14:44 +0800

    (chore) add GlobalExceptionHandler

---
+++ src/main/java/com/wanafiq/ecdsa/web/exception/AppException.java
@@ -0,0 +1,12 @@
+package com.wanafiq.ecdsa.web.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException() {
+    }
+
+    public AppException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ src/main/java/com/wanafiq/ecdsa/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.wanafiq.ecdsa.web.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleAnyException(Throwable e) {
+        return ResponseEntity.internalServerError().build();
+    }
+
+}
